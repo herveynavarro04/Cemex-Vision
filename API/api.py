56799e4ca1c870684adef338a03afad6acd5235d
herveynavarro04 <herveynavarro2004@gmail.com>
2024-11-16 20:54:26 -0600

    get url method created

--- API/api.py
+++ API/api.py
@@ -1,17 +1,16 @@
-from flask import flask, request, jsonify
+from flask import Flask, request, jsonify
 import boto3
 import os
-from io import BytesIO
 from dotenv import load_dotenv
 
 load_dotenv()
-app = flask("__api__")
+app = Flask("__name__")
 
 s3 = boto3.client(
     's3',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-    region_name=os.getenv('AWS_REGION')
+    region_name=os.getenv('us-east-1')
 )
 
 IMG_BUCKET = os.getenv('IMG_BUCKET')
@@ -19,21 +18,33 @@ MODEL_BUCKET = os.getenv('MODEL_BUCKET')
 
 @app.route('/uploadIMG', methods=['POST'])
 def upload_image():
-    if 'image' not in request.files:
-        return jsonify({"error": "No image found"}), 400
-    file = request.files['image']
-
-    if not file or file.filename:
-        return jsonify({"error": "Invalid file"}), 400 
-    
+    data = request.json
+    if not data or 'name' == '' or 'path' == '':
+        return jsonify({"error": "Bad Format"}), 400
+    file_name = data['fileName']
+    path = data['path']
     try:
-        s3.upload_fileObj(
-            file,
-            IMG_BUCKET,
-            f'uploads/{file.filename}',  
-            ExtraArgs={'ContentType': file.content_type}  
+        s3.upload_file(path, IMG_BUCKET, file_name)
+        return jsonify({"message": "File uploaded to AWS"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/getModel/<filename>', methods=['GET'])
+def get_image_url(filename):
+    try:        
+        file_url = s3.generate_presigned_url(
+            'get_object',
+            Params={
+                'Bucket': IMG_BUCKET,
+                'Key': f'uploads/{filename}'
+            },
+            ExpiresIn=3600  
         )
-        return jsonify({"message:" "File uploaded to AWS"}), 200
+        
+        return jsonify({"file_url": file_url}), 200
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+if __name__ == '__main__':
+    app.run(debug=True)
